ed4cbd4110eb65a25345b9ffa7fa460aabdc69a0
surendhar-1706 <surendhar2k18@gmail.com>
2022-01-14 18:09:21 +0530

    added daphne

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-release: python manage.py migrate
-
-web: gunicorn backend.wsgi --log-file -
+web: daphne backend.asgi:application --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker channels --settings=backend.settings -v2
--- backend/asgi.py
+++ backend/asgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+import django
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# application = get_asgi_application()
+
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
